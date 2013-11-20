471386af7b8974e7fad6fd2ff29c1bd9ff5378ec
Christian Poulwey <christian@localhost.localdomain>
2013-11-20 19:08:09 +0100

    Optimierungen der SimpleIO-Klasse
    Signed-off-by: Christian Poulwey <christian@localhost.localdomain>

--- Eliza/Eliza.java
+++ Eliza/Eliza.java
@@ -9,9 +9,9 @@ public class Eliza
 
 {
 
-public String begruessung[] = {"hi", "hallo", "guten Tag", "moin", "grüss Gott", "Servus"};
-public String verabschiedung[] = {"Tschüss", "ciao", "bye", "auf Wiedersehen", "adios", "hasta la vista"};
-public String cnf[] = {"Wie bitte", "Könntest du das nochmal sagen, ich habe es nicht verstanden", "kapier ich nicht", "was?"};
+public String begruessung[] = {"hi", "hallo", "guten Tag", "moin", "grÃ¼ss Gott", "Servus", "gib Flosse Genosse", "alles fit im Schritt?"};
+public String verabschiedung[] = {"TschÃ¼ss", "ciao", "bye", "auf Wiedersehen", "adios", "hasta la vista"};
+public String cnf[] = {"Wie bitte", "KÃ¶nntest du das nochmal sagen, ich habe es nicht verstanden", "kapier ich nicht", "was?"};
 
     public Eliza()
     {
@@ -23,16 +23,18 @@ public String cnf[] = {"Wie bitte", "Könntest du das nochmal sagen, ich habe es 
     }
     public void ai(){
         SimpleIO io = new SimpleIO();
-        String word = io.readline("CHAT>");
+        io.terminalBox("Eliza 1.0");
+        String word = " ";
         while (!(word.equals("ade"))){
+            word = io.readline("CHAT>");
+            
             if (word.equals("hi")){
-                System.out.println("Hallo auch");
+                System.out.println(begruessung[io.getRandomInt(begruessung.length, 0)]);
             }else if (word.equals("ade")){
-                System.out.println("Tschüss auch");
+                System.out.println(verabschiedung[io.getRandomInt(verabschiedung.length, 0)]);
             }else {
                 System.out.println(cnf[io.getRandomInt(cnf.length, 0)]);
             }
-             word = io.readline("CHAT>");
         }
   
         }
--- Eliza/SimpleIO.java
+++ Eliza/SimpleIO.java
@@ -1,8 +1,10 @@
 import java.io.*;
 public class SimpleIO{
 
-	//simple Method for entering text
-	  public String readline(String frage) {
+    /**
+     * simple Method for entering text
+     */
+      public String readline(String frage) {
       System.out.print(frage);
       try {
          BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(System.in));
@@ -14,13 +16,17 @@ public class SimpleIO{
          return "0";
       }
     }
-	
-	 //simply generates a random int
+    
+    /**
+     *simply generates a random int
+     */
     public int getRandomInt (int upper,int lower) {
         return (int) (Math.random() * (upper - lower) + lower);
     }
 
-    //generates a Random Boolean with the probability for true as a int param in %   
+    /**
+     *generates a Random Boolean with the probability for true as a int param in %
+     */
     public boolean getRandomBool (int probability){
         double temp = (Math.random() * 100);
         if (temp <= probability){
@@ -30,7 +36,59 @@ public class SimpleIO{
         }
         
     }
-}
-	
+    
+    /**
+     * generates a random emoticon of a defined type
+     * Types:
+     *    1 happy
+     *    2 unhappy
+     *    3 crying
+     *    4 twinkle
+     *    5 sticking tongue out
+     *    6 loud laugthing
+     *    7 being surprised
+     *    
+     */
+    public String getEmoticon (int type){
+        String happy[]={":-)",":)","=)",":]",":>",":c)","x)",":o)"};
+        String unhappy[]={":-(",":(","=(",":[",":<",":/","x(",":o(",":C"};
+        String crying[]={":'(",":'C"};
+        String twinkle[]={";-)",";)",";]",";o)"};
+        String tongue[]={":-P",":p",":b","=P",":P","dx","xP",";-P",":oP"};
+        String laugth[]={":-D",";D",":D","=D","xD","XD",":oD"};
+        String surprised[]={":-0",":-o",":o","=O",":0","=o"};
+        
+        switch(type){
+            case 1: return happy[getRandomInt(happy.length , 0 )];
+            case 2: return unhappy[getRandomInt(unhappy.length , 0 )];
+            case 3: return crying[getRandomInt(crying.length , 0 )];
+            case 4: return twinkle[getRandomInt(twinkle.length , 0 )];
+            case 5: return tongue[getRandomInt(tongue.length , 0 )];
+            case 6: return laugth[getRandomInt(laugth.length , 0 )];
+            case 7: return surprised[getRandomInt(surprised.length , 0 )];
+            default: return null;
+        
+        
+        }
+    }
+    
+    public void terminalBox (String caption){
+    
+        System.out.println("");
+        terminalLine ('*',caption.length() + 4);
+        System.out.println ("* "+caption+" *");
+        terminalLine ('*',caption.length() + 4);
+ 
+    }
 
-	
+    public void terminalLine (char c, int length){
+        for (int i=1 ; i<= length; i++){
+            System.out.print(c);
+        }
+        System.out.println("");
+    }
+   
+}
+    
+
+    
